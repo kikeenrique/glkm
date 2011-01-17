fe28d07f29d2e5c90ba936c49333765bae596b78
Enrique Garcia <github@lemambo.com>
2011-01-17 00:16:45 +0000

    2011-01-17
               * glkmd/*
                  + Autotools configuration improvements.
    
               * glkmd/test/*
               * glkmd/src/Makefile
                  + Added testing support. Moving forward on a unit testing infrastructure.
                  + Moved glkmd_test_client to glkmd/test/

---
+++ glkmd/test/MessageNetlinkTest.cpp
@@ -0,0 +1,40 @@
+/*
+ * glkmd
+ * MessageNetlinkTest.cpp
+ * Copyright (C) Enrique Garcia 2010
+ * 
+ * glkmd is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * glkmd is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "MessageNetlink.h"
+
+#include "gtest/gtest.h"
+
+// Tests factorial of 0.
+//TEST(FactorialTest, Zero) {
+//  EXPECT_EQ(1, Factorial(0));
+//}
+
+
+// Step 3. Call RUN_ALL_TESTS() in main().
+//
+// We do this by linking in src/gtest_main.cc file, which consists of
+// a main() function which calls RUN_ALL_TESTS() for us.
+//
+// This runs all the tests you've defined, prints the result, and
+// returns 0 if successful, or 1 otherwise.
+//
+// Did you notice that we didn't register the tests?  The
+// RUN_ALL_TESTS() macro magically knows about all the tests we
+// defined.  Isn't this convenient?
